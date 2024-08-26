3b1cee5db5258f53ee33219ab69ed26e10bf83db
INDavid04 <davidirimia04@gmail.com>
2024-08-26 15:58:40 +0300

    fix: previous problem solution - mongoose not mangoose :)

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 if (process.env.NODE_ENV !== 'production') {
-    require('dotenv').parse()
+    require('dotenv').config()
 }
 
 const express = require('express')
@@ -14,9 +14,9 @@ app.set('layout', 'layouts/layout')
 app.use(expressLayouts)
 app.use(express.static('public'))
 
-const mangoose = require('mangoose')
-mangoose.connect(process.env.DATABASE_URL, {useNewUrlParser: true })
-const db = mangoose.connection
+const mongoose = require('mongoose')
+mongoose.connect(process.env.DATABASE_URL)
+const db = mongoose.connection
 db.on('error', error => console.error(error))
 db.once('open', () => console.log('Connected to Mongoose'))
 
